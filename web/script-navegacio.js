23fd47d173ee456767d670f6cfa4efa938fb0c4b
unknown <ziheng0830@gmail.com>
2020-04-01 11:58:00 +0200

    Primera entrega

--- web/script-navegacio.js
+++ web/script-navegacio.js
@@ -33,7 +33,7 @@ class ObjNavegacio {
         // S'amaga la seccio que estava visible i es mostra la que s'ha demanat
         refActual.style.display = 'none'
         refNova.style.display = 'flex'
-        
+
         // La seccio actual passa a ser la que s'ha demanat
         this.seccioActual = seccioNova
 
@@ -49,3 +49,39 @@ class ObjNavegacio {
 }
 
 
+
+function mostraMenu (evt) {
+
+    let refBody = document.getElementsByTagName('body')[0],
+        refSmall = document.getElementById('frontendMenuMobil'),
+        refContainer = document.getElementById('menuContainer'),
+        estilSmall = window.getComputedStyle(refSmall, ''),
+        estilContainer = window.getComputedStyle(refContainer, ''),
+        midaContainer = parseInt(estilContainer.getPropertyValue('height')),
+        altura = - midaContainer + 10
+
+
+    refBody.style.overflow = 'hidden' // Treure scroll
+    refSmall.style.visibility = 'visible'
+    refSmall.style.display = 'flex';
+    refSmall.style.opacity = 1
+
+    refContainer.style.transform =  'translateY(' + altura + 'px)'
+}
+function amagaMenu (evt) {
+    let refBody = document.getElementsByTagName('body')[0],
+        refSmall = document.getElementById('frontendMenuMobil'),
+        refContainer = document.getElementById('menuContainer')
+
+    refBody.style.overflow = 'auto' // Recuperar scroll
+
+    refSmall.style.opacity = 0
+    setTimeout(() => { refSmall.style.visibility = 'hidden' }, 500)
+
+    refContainer.style.transform = 'translateY(0)'
+}
+function navega (evt, lloc) {
+    evt.stopPropagation() // Evitar que executi 'amagaMenu' des de 'menuSmall'
+    //location.href = lloc
+    console.log('Navegar a ', lloc)
+}
